ce00de0e29cbf6b19a79cecd67b55b4ba42689e7
Daniela Martin <frau.daniela.martin@gmail.com>
2018-11-17 13:32:17 -0800

    Added sceleton for main function

--- SystemCallsBase/MiniOS/src/main.c
+++ SystemCallsBase/MiniOS/src/main.c
@@ -6,9 +6,11 @@
 #include "minios/hal/hal.h"
 #include "minios/console/console.h"
 
+bool gameover = false;
+
 int main(void)
 {
-	//Initializes the system
+	/*//Initializes the system
 	system_init();		
 	
 	//Execute the console
@@ -17,6 +19,31 @@ int main(void)
 
 	while(1); //if main returns we want to stop here...
 			  //else garbage gets executed.
-			  //Q: why? (Hint: Picture machine code in memory)
+			  //Q: why? (Hint: Picture machine code in memory)*/
+	
+	while (true)
+	{
+		//display start screen
+		// press any button to start
+		
+		// wait for button to be pressed
+		
+		// initially display game state
+		
+		while (!gameover)
+		{
+			// do a delay
+			hal_delay(2000);
+			
+			// was a button pressed 
+			// if yes handle it
+			
+			// display the new game stat
+		}
+		
+		// game is over -> display gameoverstate
+		
+		// wait for button to be pressed for a new game state
+	}
 }
 

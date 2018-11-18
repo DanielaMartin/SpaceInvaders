49c5853b45aa031d252c21a8c94e586bbd645d58
Daniela Martin <frau.daniela.martin@gmail.com>
2018-11-17 16:29:42 -0800

    added creation of aliens-

--- SystemCallsBase/MiniOS/Debug/src/main.d
+++ SystemCallsBase/MiniOS/Debug/src/main.d
@@ -2,7 +2,8 @@ src/main.d src/main.o: ../src/main.c ../src/minios/system.h \
  ../src/minios/hal/hal.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stdint-gcc.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stdbool.h \
- ../src/minios/console/console.h ../src/space_inv.h
+ ../src/minios/console/console.h ../src/space_inv.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h
 
 ../src/minios/system.h:
 
@@ -15,3 +16,5 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gc
 ../src/minios/console/console.h:
 
 ../src/space_inv.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\arm\arm-gnu-toolchain\lib\gcc\arm-none-eabi\6.3.1\include\stddef.h:
--- SystemCallsBase/MiniOS/src/main.c
+++ SystemCallsBase/MiniOS/src/main.c
@@ -6,8 +6,12 @@
 #include "minios/hal/hal.h"
 #include "minios/console/console.h"
 #include "space_inv.h"
+#include <stddef.h>
+#include <stdbool.h>
 
 #define ALIEN_NUM 15
+//#define NULL 0
+
 bool gameover = false;
 bool button_pressed = false;
 tButtonNum seleced_button;
@@ -16,6 +20,8 @@ tButtonNum seleced_button;
 Shape spaceship;
 Shape aliens [ALIEN_NUM];
 Shape bullets;
+bool hasBullet = false;
+
 
 
 void button_callback (tButtonNum);
@@ -24,14 +30,6 @@ int main(void)
 {
 	//Initializes the system
 	system_init();		
-	
-	/*//Execute the console
-	console_begin( "\n\r" ); //Some serial console like putty use \n\r as newlines,
-							 //others like Atmel's Terminal use only \n. Choose accordingly.
-
-	while(1); //if main returns we want to stop here...
-			  //else garbage gets executed.
-			  //Q: why? (Hint: Picture machine code in memory)*/
 	
 	// start button
 	hal_button_start_int(button_callback);
@@ -44,6 +42,16 @@ int main(void)
 		// wait for button to be pressed
 		
 		// initially display game state
+		// create all the aliens
+		// a block of 3*5 aliens at the top right corner of the screen
+		// start at position (10,0)
+		uint16_t x = 0;
+		for (uint16_t i = 0; i < 15; i++)
+		{
+			//TODO: check if this really works
+			aliens[i] = make_shape(alien, x + 10, i % 4);
+			x = (i % 4 == 3) ? x+1 : x;
+		}
 		
 		while (!gameover)
 		{
@@ -62,6 +70,16 @@ int main(void)
 						break;
 					case  Button2:
 						// shoot a bullet from the position of the space ship
+						// is there a bullet already? if yes do not shoot.
+						// if no shoot
+						if (!hasBullet)
+						{
+							// create a new bullet
+							// set bullet position
+							// it should be the position right to the current position of the spaceship 
+							bullets = make_shape(bullet, (spaceship.x + 1), spaceship.y);
+							hasBullet = true;
+						}
 						break;
 					case Button3:
 						// move the space ship down wards
@@ -73,6 +91,10 @@ int main(void)
 				}
 				button_pressed = false;
 			}
+			
+			// advance bullets
+			
+			// check for collisions
 			
 			// display the new game stat
 			// including move all existing bullets one step to the right
